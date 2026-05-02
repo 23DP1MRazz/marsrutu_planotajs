5804bcc922ed3c3dc051439acb8dfa9fd6467b1c
Marats Razzivins <A230372MR@rvt.lv>
2026-05-02 19:54:49 +0300

    proxy fix

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,6 +18,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
+
         $middleware->encryptCookies(except: ['appearance', 'locale', 'sidebar_state']);
 
         $middleware->web(append: [
